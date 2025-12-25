aa62f19d95e9ec47c7282d125867eec55b06127e
Farah <farahhanyy766@gmail.com>
2025-12-25 18:09:25 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:cafe_restaurant_guide/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        LoginScreen.routeName : (context) => LoginScreen(),
+      },
+    );
+  }
+}
+
